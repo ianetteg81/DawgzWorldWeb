e03f5bed8388185b1e9560b3b5257cf4a5f7caa3
Ianette Grady <54339016+ianetteg81@users.noreply.github.com>
2019-10-10 14:39:37 -0500

    Add files via upload

---
+++ DawgHouse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DawgzWorldWeb.Data
+{
+    public class DawgHouse
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DHID { get; set; }
+
+        [Display(Name ="DawgHouse Id")]
+        public string DHName { get; set; }
+
+       
+        [ForeignKey("PDID")]
+        public PresiDawgs PresiDawgsId { get; set; }
+    }
+}
---
+++ PresiDawgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DawgzWorldWeb.Data
+{
+    public class PresiDawgs
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PDID { get; set; }
+
+        [Display(Name = "PresdiDawg Id")]
+        public string PDName { get; set; }
+
+
+        [ForeignKey("DHID")]
+        public DawgHouse DawgHouseID { get; set; }
+
+
+
+    }
+}
